22b2ac298ef843d960da6c1a3c5151d2633dbe63
confused_techie <dev@lhbasics.com>
2025-06-30 14:59:27 -0700

    Greatly advance site building script and content

--- scripts/build_site.js
+++ scripts/build_site.js
@@ -19,7 +19,6 @@ class BuildFile {
     this.file = file; // The raw file input
     this.ext = ext; // The file extension of the original file
     this.layoutDir = "./site/_layouts";
-
     // Page Data
     this.content; // The first pass processing of the file content, prior to being templated
     this.layout; // The layout that should be used for templating, if one is defined
@@ -35,6 +34,12 @@ class BuildFile {
       if (this.data.layout) {
         this.layout = this.data.layout;
       }
+    } else if (this.ext === ".js" || this.ext === ".jsonld") {
+      this.content = JSON.parse(this.file);
+    } else {
+      // If we don't recognize the extension, assign the file as content, hope
+      // it's already been parsed
+      this.content = this.file;
     }
 
     // After application specific parsing happens, we can utilize templates to
@@ -43,7 +48,9 @@ class BuildFile {
       // We will only template the content if we have a layout defined
       const templatePath = path.join(this.layoutDir, this.layout);
       const template = fs.readFileSync(templatePath, { encoding: "utf-8" });
-      this.output = ejs.render(template, this); // TODO support more than EJS maybe
+      this.output = ejs.render(template, this, {
+        views: [path.resolve(this.layoutDir)]
+      }); // TODO support more than EJS maybe
     } else {
       // We will just return the raw page content as the final output
       this.output = this.content;
@@ -52,6 +59,26 @@ class BuildFile {
 }
 
 class BuildSite {
+  /**
+   * Named ModDir after the Apache config option of the same name that enables
+   * 'Provides for "traling slash" redirects and serving directory index files'
+   * Basically, allowing an 'index.html' file to be served automatically when
+   * a user redirects to it's directory. This method will take the given file
+   * and create it's top level directory and then write it's contents to an index
+   * file.
+   */
+  static WriteApacheModDir(outTarget, content) {
+    const outTargetSansExt = outTarget.replace(path.extname(outTarget), "");
+    // TODO check for this folder existing first, but pretty sure the function
+    // will fail by default if that's the case
+    fs.mkdirSync(outTargetSansExt);
+    fs.writeFileSync(
+      path.join(outTargetSansExt, `index.html`),
+      content,
+      { encoding: "utf-8" }
+    );
+  }
+
   constructor(dir) {
     this.input = dir;
     this.output = path.resolve("./_dist");
@@ -64,14 +91,7 @@ class BuildSite {
         func: (input) => {
           const file = new BuildFile(input, ".md");
           file.build();
-          console.log(file);
           return file.output;
-        }
-      },
-      ".txt": {
-        out_ext: ".html",
-        func: (input) => {
-          return input;
         }
       }
     };
@@ -134,16 +154,11 @@ class BuildSite {
             fs.writeFileSync(`${outTargetSansExt}${this.exts[ext].out_ext}`, output, { encoding: "utf-8" });
           } else {
             // Place output in it's own folder as the index file
-
-            fs.mkdirSync(outTargetSansExt);
-            fs.writeFileSync(
-              path.join(outTargetSansExt, `index${this.exts[ext].out_ext}`),
-              output,
-              { encoding: "utf-8" }
-            );
+            this.WriteApacheModDir(outTarget, output);
           }
         } else {
-          console.error(`Unable to find handler for file: ${target}`);
+          console.error(`Unable to find handler for file: "${target}" copying in full.`);
+          fs.copyFileSync(target, outTarget);
         }
       }
     }
@@ -153,3 +168,34 @@ class BuildSite {
 const build = new BuildSite("./site");
 
 build.build();
+
+// Now with all the normal content having been built, the JSON-LD data can be
+// triggerred manually
+
+const jsonLdDataRaw = fs.readFileSync("./generated/schema.jsonld", { encoding: "utf-8" });
+const jsonLdFile = new BuildFile(jsonLdDataRaw, ".json");
+jsonLdFile.layout = "jsonld_tree.ejs";
+jsonLdFile.build();
+
+// Then the full tree will be written as an index file, rather than .html
+// so the parent folder must be made
+const outTargetSansExt = path.join(build.output, "hierarchy");
+BuildSite.WriteApacheModDir(outTargetSansExt, jsonLdFile.output);
+
+// Then to build the individual pages for each node of our JSON-LD graph
+
+const jsonLdData = JSON.parse(jsonLdDataRaw);
+
+for (const node of jsonLdData["@graph"]) {
+  const nodeFile = new BuildFile(node, "");
+  nodeFile.layout = "jsonld_node.ejs";
+  nodeFile.build();
+
+  const outNodeTargetSansExt = path.join(build.output, node["rdfs:label"]);
+  BuildSite.WriteApacheModDir(outNodeTargetSansExt, nodeFile.output);
+}
+
+// Finally manually move over all data files
+fs.copyFileSync("./generated/schema.jsonld", path.join(build.output, "schema.jsonld"));
+fs.copyFileSync("./generated/schema.rdf", path.join(build.output, "schema.rdf"));
+fs.copyFileSync("./data/schema.ttl", path.join(build.output, "schema.ttl"));
